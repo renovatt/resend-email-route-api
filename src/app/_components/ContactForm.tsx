30a3b9d501c2b3260e504364f570684a2f587182
will <wlymes@gmail.com>
2024-02-08 15:03:24 -0300

    refactor: exported schemas type

--- src/app/_components/ContactForm.tsx
+++ src/app/_components/ContactForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { FormSchemaType, formSchema } from "@/schemas"
 
 import {
   Form,
@@ -23,26 +23,6 @@ import {
   SelectContent,
   SelectItem
 } from "@/components/ui/select";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "O nome de usuário deve ter pelo menos 2 caracteres.",
-  }),
-  email: z.string().email({
-    message: "Precisa ser um e-mail válido.",
-  }),
-  visitor: z.enum(["cliente", "recrutador"], { required_error: "Precisa selecionar uma opção." }),
-  message: z
-    .string()
-    .min(10, {
-      message: "A mensagem deve ter pelo menos 10 caracteres.",
-    })
-    .max(400, {
-      message: "A mensagem é muito longa.",
-    }),
-})
-
-type FormSchemaType = z.infer<typeof formSchema>
 
 export function ContactForm() {
   const { toast } = useToast()
